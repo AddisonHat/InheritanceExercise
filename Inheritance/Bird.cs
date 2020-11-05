62dfd6608c71bc39963e2cc7b617c3c33e549c90
Addison Hatfield <addisonhat@hotmail.com>
2020-11-05 16:37:21 -0600

    added Bird and Reptile classes

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Bird : Animal
+    {
+        public int Wings { get; set; }
+        public string Type { get; set; }
+        public bool IsWarmBlooded { get; set; }
+        public bool HasFeathers { get; set;}
+
+        public Bird()
+        {
+
+        }
+
+        public Bird(string name, int age, int legs, bool hasBackbone, bool isWarmBlodded ,bool hasFeathers, int wings, string type)
+        {
+            Name = name;
+            Age = age;
+            Legs = Legs;
+            HasBackbone = hasBackbone;
+            IsWarmBlooded = isWarmBlodded;
+            HasFeathers = hasFeathers;
+            Wings = wings;
+            Type = type;
+        }
+
+
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Cache;
 
 namespace Inheritance
 {
@@ -34,6 +35,48 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird articuno = new Bird()
+            {
+                Name = "Articuno",
+                Age = 1000,
+                Legs = 2,
+                HasBackbone = true,
+                IsWarmBlooded = false,
+                HasFeathers = true,
+                Wings = 2,
+                Type = "Ice",
+
+
+
+
+
+            };
+
+            Console.WriteLine($"{articuno.Name} is an {articuno.Type } pokemon that is {articuno.Age} years old and has {articuno.Legs} legs and {articuno.Wings} wings");
+            Console.WriteLine($"is {articuno.Name} a vertebrate, warm blooded and has feathers?  {articuno.HasBackbone} {articuno.IsWarmBlooded} {articuno.HasFeathers}");
+            Console.WriteLine();
+
+
+
+            Reptile dragon = new Reptile()
+            {
+                Name = "Eragon",
+                Age = 500,
+                Legs = 4,
+                HasBackbone = true,
+                IsColdBlooded = true,
+                HasScales = true,
+                Enviroment = "Mountains",
+                Defense = "Breathes Fire",
+            };
+
+            Console.WriteLine($"{dragon.Name} is {dragon.Age} years old it has {dragon.Legs} and {dragon.Defense} it lives in the {dragon.Enviroment}");
+            Console.WriteLine($"is {dragon.Name} cold blooded also does {dragon.Name} have scales and a backbone  {dragon.IsColdBlooded}  {dragon.HasScales}  {dragon.HasBackbone}");
+
+
+
+
+
         }
     }
 }
